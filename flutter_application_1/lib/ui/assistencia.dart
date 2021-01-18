d9da8cf2b724411f5ee98d5c6a7473f9e4fddaf8
Guilherme Machado <gui__machado@hotmail.com>
2021-01-18 17:38:13 -0300

    todas as listas nos lugares

---
+++ flutter_application_1/lib/ui/assistencia.dart
@@ -0,0 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Assistencia extends StatefulWidget {
+  Assistencia({Key key}) : super(key: key);
+
+  @override
+  AssistenciaState createState() => AssistenciaState();
+}
+
+class AssistenciaState extends State<Assistencia> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Assiência"),
+            backgroundColor: Colors.deepPurpleAccent[100]),
+        body: new StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('noticias').snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return CircularProgressIndicator();
+              } else {
+                List<QueryDocumentSnapshot> docs = snapshot.data.docs;
+                return new ListView.builder(
+                    itemCount: docs.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                          child:
+                              build_card(context, docs[index].data().values));
+                    });
+              }
+            }));
+  }
+
+  Widget build_card(BuildContext context, Iterable<dynamic> dadosNoticias) {
+    return Card(
+        child: Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            child: Expanded(flex: 1, child: new Image.asset('icone.PNG')),
+          ),
+          Expanded(
+            flex: 3,
+            child: Column(children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListTile(
+                  title: Text(dadosNoticias.elementAt(1),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueAccent.withOpacity(0.8))),
+                  subtitle: Text(
+                    dadosNoticias.elementAt(0),
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ),
+              ),
+              FlatButton(
+                textColor: Colors.blue,
+                padding: EdgeInsets.all(8.0),
+                onPressed: () {
+                  print("clicado");
+                },
+                child: Text(
+                  "Ler mais ->",
+                  style: TextStyle(fontSize: 15.0),
+                ),
+              )
+            ]),
+          )
+        ],
+      ),
+    ));
+  }
+}
--- flutter_application_1/lib/ui/bottonTabs.dart
+++ flutter_application_1/lib/ui/bottonTabs.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/ui/assistencia.dart';
+import 'package:flutter_application_1/ui/identificacao.dart';
 import 'package:flutter_application_1/ui/lista_de_noticias.dart';
+import 'package:flutter_application_1/ui/monitoramento.dart';
+import 'package:flutter_application_1/ui/registro.dart';
 
 /// This is the stateful widget that the main application instantiates.
 class Tabs extends StatefulWidget {
@@ -16,26 +20,10 @@ class _MyStatefulWidgetState extends State<Tabs> {
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   List<Widget> _widgetOptions = <Widget>[
     ListaDeNoticias(),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 4: School',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 5: School',
-      style: optionStyle,
-    ),
+    Identificacao(),
+    Assistencia(),
+    Monitoramento(),
+    Registro(),
   ];
 
   void _onItemTapped(int index) {
@@ -57,23 +45,23 @@ class _MyStatefulWidgetState extends State<Tabs> {
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-              icon: Icon(Icons.home),
+              icon: Icon(Icons.announcement),
               label: 'Notícias',
               backgroundColor: Color.fromARGB(500, 75, 66, 121)),
           BottomNavigationBarItem(
-              icon: Icon(Icons.business),
+              icon: Icon(Icons.person_search),
               label: 'Identificação',
               backgroundColor: Color.fromARGB(500, 75, 66, 121)),
           BottomNavigationBarItem(
-              icon: Icon(Icons.school),
+              icon: Icon(Icons.assistant),
               label: 'Assistência',
               backgroundColor: Color.fromARGB(500, 75, 66, 121)),
           BottomNavigationBarItem(
-              icon: Icon(Icons.school),
+              icon: Icon(Icons.monitor),
               label: 'Monitoramento',
               backgroundColor: Color.fromARGB(500, 75, 66, 121)),
           BottomNavigationBarItem(
-              icon: Icon(Icons.school),
+              icon: Icon(Icons.report),
               label: 'Registro',
               backgroundColor: Color.fromARGB(500, 75, 66, 121)),
         ],
